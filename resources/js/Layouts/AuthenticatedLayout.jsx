ecfc41f65f4ebd015b1d39c78136dda660400e18
Leons Aleksandrovs <skrazzo@proton.me>
2024-02-28 21:16:38 +0200

    Nu tagad chista krasas strada

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -5,6 +5,7 @@ import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
 import s from '@/Components/scss/components.module.css';
+import { IconDeviceGamepad2, IconLayoutDashboard } from '@tabler/icons-react';
 
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
@@ -23,7 +24,7 @@ export default function Authenticated({ user, header, children }) {
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                                    Dashboard
+                                    <IconLayoutDashboard /> Dashboard
                                 </NavLink>
                                 
                                 <NavLink href={route('play')} active={route().current('play')}>
@@ -72,7 +73,7 @@ export default function Authenticated({ user, header, children }) {
                         <div className="-me-2 flex items-center sm:hidden">
                             <button
                                 onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
-                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
+                                className={`inline-flex items-center justify-center p-2 rounded-md ${s.secondary_text} ${s.background}`}
                             >
                                 <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                     <path
@@ -98,7 +99,11 @@ export default function Authenticated({ user, header, children }) {
                 <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
-                            Dashboard
+                            <IconLayoutDashboard className={`${s.nav_icon}`}/> Dashboard
+                        </ResponsiveNavLink>
+
+                        <ResponsiveNavLink href={route('play')} active={route().current('play')}>
+                            <IconDeviceGamepad2 className={`${s.nav_icon}`}/> Play
                         </ResponsiveNavLink>
                     </div>
 
