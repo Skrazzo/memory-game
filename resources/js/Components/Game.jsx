652f66ed1430bac6fb5cd0c3b8f021b06bd3be42
Leons Aleksandrovs <skrazzo@proton.me>
2024-03-04 17:23:39 +0200

    right button animation

--- resources/js/Components/Game.jsx
+++ resources/js/Components/Game.jsx
@@ -6,76 +6,87 @@ import GameTile from "./GameTile";
 
 
 export default function Game() {
-    const [level, setLevel] = useState(2);
-    const [rightTiles, setRightTiles] = useState([]);
-    const [cfg, setCFG] = useState({ // default cfg for first level
-        layout: 3,
-        right_tiles: 3
-    });
+    const [level, setLevel] = useState(0);
+    const [mounted, setMounted] = useState(false);
+    const [showAnim, setShowAnim] = useState(false);
 
-    function generate_rand_tiles(x, y, count) {
-        const arr = [];
-        while (arr.length < count) {
-            const rand_num = Math.floor(Math.random() * (y - x + 1)) + x;
-            if (!arr.includes(rand_num)) {
-                arr.push(rand_num);
+    // function to generate random right tile positions
+    function generate_rand_tiles(x, count) {
+        const numbers = [];
+        while (numbers.length < count) {
+            const randNum = Math.floor(Math.random() * x) + 1;
+            if (!numbers.includes(randNum)) {
+                numbers.push(randNum);
             }
         }
-        return arr;
+        return numbers;
     }
 
+    const [cfg, setCFG] = useState({ // default cfg for first level
+        layout: 3,
+        right_tiles: 3,
+        right_tiles_places: generate_rand_tiles(9, 3)
+    });
+
     function return_tile_place(x, y, layout = 3){
-        //console.log( `X: ${x} and Y: ${y}`);
         return ((layout * y) + (1 * x)) + 1;
     }
 
     function generate_tiles(){
-        setRightTiles(generate_rand_tiles(1, (cfg.layout * cfg.layout), cfg.right_tiles));
+        setRightTiles(generate_rand_tiles((cfg.layout * cfg.layout), cfg.right_tiles));
     }
 
     function calculate_cfg(){
+        if(!mounted) {
+            level_up();
+            setMounted(true);
+            return ;
+        }
+
         if(level < 1){
             setLevel(1);
             return ;
         }
 
+        const newCFG = {
+            layout: 3 + Math.floor(level / 3),
+            right_tiles: 2 + level,
+        };
+
         setCFG({
-            layout: 3 + Math.floor(level / 2),
-            right_tiles: 2 + level
+            ...newCFG,
+            right_tiles_places: generate_rand_tiles((newCFG.layout * newCFG.layout), newCFG.right_tiles),
         });
+
+        setTimeout(() => {
+            setShowAnim(true);
+            setTimeout(() => setShowAnim(false), 1000);
+        }, 500);
         
     }
+
+    function level_up(){
+        setLevel(level + 1);
+    }
+
     useEffect(calculate_cfg, [level]);
     useEffect(() => console.log('cfg', cfg), [cfg]);
-    useEffect(() => console.log('rightTiles', rightTiles), [rightTiles]);
     
-    // (1 * i) + (1 * j) + 1
-    /*
-      # [] []
-     [] [] []
-     [] [] []
-    (1 * 0) + (1 * 0) + 1
 
-     []  # []
-     [] [] []
-     [] [] []
-     (1 * 0) + (1 * 1)  + 1
-
-     [] [] []
-      # [] []
-     [] [] []
-     (1 * 1) + (1 * 0) + 1
-    */
     return (
         <div>
-            <button onClick={() => generate_tiles()} className="m-1 bg-blue-500 p-2 rounded text-white">Gen</button>
+             <button onClick={() => setLevel(level - 1)} className="m-1 bg-blue-500 p-2 rounded">lower</button>
+            <button onClick={level_up} className="m-1 bg-blue-500 p-2 rounded">up</button>
             
             <div className={`${s.container} gap-${(cfg.layout >= 5) ? '1' : '3'}`}>
                 {Array.from({ length: cfg.layout }, (x, i) => {
                     return (
                         <div key={i} className={`${s.row} gap-${(cfg.layout >= 5) ? '1' : '3'}`}>
-                            {Array.from({ length: cfg.layout }, (q, j) => { 
-                                return <GameTile place={return_tile_place(j, i, cfg.layout)} key={`${i}-${j}`}/>;
+                            {Array.from({ length: cfg.layout }, (q, j) => {
+                                // Calculation for a game tile
+                                const tile_place = return_tile_place(j, i, cfg.layout);
+
+                                return <GameTile showAnim={showAnim} right={(cfg.right_tiles_places.includes(tile_place)) ? true : false} key={`${i}-${j}`}/>;
                             })}
                         </div>
                     );
--- resources/js/Components/GameTile.jsx
+++ resources/js/Components/GameTile.jsx
@@ -1,7 +1,9 @@
 import s from '@/Components/scss/game.module.css';
+import { useEffect, useState } from 'react';
 
-export default function GameTile({ right = false, onClick = () => console.log(right), place }) {
+export default function GameTile({ right = false, onClick = () => console.log(right), showAnim = false}) {
+   
     return (
-        <div className={`${s.tile}`} onClick={onClick}>{place}</div>
+        <div className={`${s.tile} ${(showAnim && right) ? s.right_anim : ''}`} onClick={onClick}> </div>
     )
 }
